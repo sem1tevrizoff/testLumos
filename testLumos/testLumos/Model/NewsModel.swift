72533f7564a5da08aba300ddef40e119abfd7331
sem1tevrizoff <pasqoooda@gmail.com>
2023-02-01 12:46:52 +0300

    no message

---
+++ testLumos/testLumos/Model/NewsModel.swift
@@ -0,0 +1,23 @@
+//
+//  NewsModel.swift
+//  testLumos
+//
+//  Created by sem1 on 1.02.23.
+//
+
+import Foundation
+
+struct NewsModel: Codable {
+    
+    let articles: [Articles]
+    
+    struct Articles: Codable {
+        
+        let author: String?
+        let title: String
+        let description: String
+        let publishedAt: String
+        let content: String
+        
+    }
+}
